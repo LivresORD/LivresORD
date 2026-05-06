acf2a24493a3203c41e165e7d6a6cb795318f9b5
RL <lourap24@ecolecatholique.ca>
2026-05-06 14:55:35 -0400

    hello

--- LivresORD_project/src/VueBibliothecaire.java
+++ LivresORD_project/src/VueBibliothecaire.java
@@ -2,13 +2,22 @@ import javax.swing.*;
 
 public class VueBibliothecaire extends JFrame {
     private JLabel TitreLabel = new JLabel("Gestion des Livres", JLabel.CENTER);
+    private JButton ajouterLivreButton = new JButton("Ajouter un Livre");
+    private JButton supprimerLivreButton = new JButton("Supprimer un Livre");
+    private JButton modifierLivreButton = new JButton("Modifier un Livre");
+    private JPanel buttonPanel = new JPanel();
 
     public VueBibliothecaire() {
         setTitle("LivresORD - Bibliothécaire");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(400, 300);
+        setSize(500, 300);
         setLocationRelativeTo(null);
         add(TitreLabel);
+        buttonPanel.add(ajouterLivreButton);
+        buttonPanel.add(supprimerLivreButton);
+        buttonPanel.add(modifierLivreButton);
+        add(buttonPanel, "South");
+
 
     }
     public static void main(String[] args) {
